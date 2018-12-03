1ecbe2451952805702a886f9d5f6d0671a0a2346
Eggert Geir Axelsson <eggertgeir@gmail.com>
2018-12-03 15:03:21 +0000

    Add files via upload

--- Lokaverkefni/Sjoppa.h
+++ Lokaverkefni/Sjoppa.h
@@ -1,8 +1,19 @@
 #pragma once
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using namespace std;
 class Sjoppa
 {
+private:
+	string item;
+	int verd;
+	string deild;
 public:
-	Sjoppa();
-	~Sjoppa();
+	Sjoppa() : item(""), verd(0), deild(""){};
+	Sjoppa(string item, int verd, string deild) : item(item), verd(verd), deild(deild){};
+	
 };
 
