679819162c2354914b4a1f9b6a812accc9f894b7
elas99 <elas99@hotmail.com>
2018-12-05 08:49:40 +0000

    Add files via upload

--- Lokaverkefni/Lokaverkefni.cpp
+++ Lokaverkefni/Lokaverkefni.cpp
@@ -15,7 +15,7 @@ private:
 	string nafn;
 	int afslattur;
 public:
-	Malarar() : kt(""), nafn(""), afslattur(0){};
+	Malarar() : kt(""), nafn(""), afslattur(10){};
 	Malarar(string kt, string nafn, int afslattur) : kt(kt), nafn(nafn), afslattur(afslattur){};
 	string getKT() { return kt; };
 	void print(){
@@ -29,11 +29,11 @@ private:
 	string nafn;
 	int afslattur;
 public:
-	Rafverktakar() : kt(""),nafn(""), afslattur(0){};
+	Rafverktakar() : kt(""),nafn(""), afslattur(15){};
 	Rafverktakar(string kt, string nafn, int afslattur) : kt(kt), nafn(nafn), afslattur(afslattur){};
 	string getKT() { return kt; };
 	void print(){
-		cout <<kt << " : " << nafn << " : " << afslattur << "%" << endl;
+		cout << kt << " : " << nafn << " : " << afslattur << "%" << endl;
 	};
 };
 class Starfsmenn{
@@ -42,13 +42,13 @@ private:
 	string nafn;
 	int afslattur;
 public:
-	Starfsmenn() : kt(""), nafn(""), afslattur(0){};
+	Starfsmenn() : kt(""), nafn(""), afslattur(20){};
 	Starfsmenn(string kt, string nafn, int afslattur) : kt(kt), nafn(nafn), afslattur(afslattur){};
 	string getKT() { return kt; };
 	void print(){	
 		cout << kt << " : " << nafn << " : " << afslattur << "%" << endl;	
 	};
-};
+};/*
 class Vidskiptavinir{
 private:
 	Malarar *mal;
@@ -67,16 +67,10 @@ public:
 		raf = new Rafverktakar[10];
 		starf = new Starfsmenn[10];
 
-		if (afslattur == 10)
-		{
-			for (int i = 0; i < 10; i++)
-			{
-				mal[i];
-			}
-		}
+
 	};	
 };
-
+*/
 
 
 class Node {
@@ -126,7 +120,7 @@ public:
 	{
 		if (ktExists(kt, afslattur))
 		{
-			cout << "Cannot perform this operation: Flight with that id already exists" << endl;
+			cout << "Einhvad for vitlaust" << endl;
 			return;
 		}
 		if (head == nullptr)
@@ -195,9 +189,11 @@ public:
 					prev->next = temp;
 					temp->next = current;
 				}
+				
 			}
 		}
 	}
+	
 	/*void deleteNode(int id,int af)
 	{
 	Node *current = new Node;
@@ -238,24 +234,28 @@ public:
 	void display()
 	{
 		Node *temp = new Node();
+		Node *temp1 = new Node();
+		Node *temp2 = new Node();
 		temp = head;
+		temp1 = head;
+		temp2 = head;
 		while (temp != nullptr)
 		{
 			temp->Mal.print();
 			temp = temp->next;
+			
 		}
-		temp = head;
-		while (temp != nullptr)
+		while (temp2 != nullptr)
 		{
-			temp->Raf.print();
-			temp = temp->next;
+			temp2->Raf.print();
+			temp2 = temp2->next;
+
 		}
-		temp = head;
-		while (temp != nullptr)
-		{
-			temp->Starf.print();
-			temp = temp->next;
-		}
+		
+	}
+	void print()
+	{
+
 	}
 	bool ktExists(string kt, int afslattur)
 	{
@@ -310,24 +310,27 @@ public:
 };
 
 
-int main()
-{	
-	Sjoppa shop[10];
-	shop[0] = Sjoppa("Pensill            ", 999,"  Malningadeild");
-	shop[1] = Sjoppa("Blondud malning 1L ",  1500," Malningadeild");
-	shop[2] = Sjoppa("Blondud malning 5L ", 6500," Malningadeild");
-	shop[3] = Sjoppa("Blondud malning 10L", 11490,"Malningadeild");
-	shop[4] = Sjoppa("Vir 1M             ", 300,"   Rafindadeild");
-	shop[5] = Sjoppa("Netsnura 5M        ", 1500,"  Rafindadeild");
-	shop[6] = Sjoppa("HDMI Snura 10M     ", 2000,"  Rafindadeild");
-	shop[7] = Sjoppa("Klo                ", 500,"   Rafindadeild");
-	shop[8] = Sjoppa("Tenglarenna        ", 300000,"Rafindadeild");
-	shop[9] = Sjoppa("Twix               ", 999,"        Almennt");
-	for (int i = 0; i < 10; i++)
-	{
-		shop[i].print();
+class Shoppa{
+private:
+	string item;
+	int verd;
+	string deild;
+public:
+	Shoppa() : item(""), verd(0), deild(""){};
+	Shoppa(string item, int verd, string deild) : item(item), verd(verd), deild(deild){};
+	void print(){
+		cout << item << " " << verd << "kr " << deild << endl;
 	}
-	cout << "----------------" << endl;
+};
+
+int main()
+{
+	Malarar mal;
+	
+	int adgangur;
+	string kennitala;
+	int kaupa;
+	int total = 0;
 	LinkedList b = LinkedList();
 	b.createNode("2308992829", "Hilmar", 10);
 	b.createNode("2711912029", "Elas", 15);
@@ -337,8 +340,115 @@ int main()
 	b.createNode("3333333333", "Elas", 15);
 	b.createNode("0405994799", "Hilmar", 10);
 	b.createNode("2222222222", "Elas", 15);
-	b.display();
 	
-	return 0;
-}
+	Shoppa shop[10];
+	shop[0] = Shoppa("1. Pensill            ", 999, "  Malningadeild");
+	shop[1] = Shoppa("2. Blondud malning 1L ", 1500, " Malningadeild");
+	shop[2] = Shoppa("3. Blondud malning 5L ", 6500, " Malningadeild");
+	shop[3] = Shoppa("4. Blondud malning 10L", 11490, "Malningadeild");
+	shop[4] = Shoppa("5. Vir 1M             ", 300, "   Rafindadeild");
+	shop[5] = Shoppa("6. Netsnura 5M        ", 1500, "  Rafindadeild");
+	shop[6] = Shoppa("7. HDMI Snura 10M     ", 2000, "  Rafindadeild");
+	shop[7] = Shoppa("8. Klo                ", 500, "   Rafindadeild");
+	shop[8] = Shoppa("9. Tenglarenna        ", 300000, "Rafindadeild");
+	shop[9] = Shoppa("10.Twix               ", 999, "        Almennt");
+
+
+
+	cout << "1. Login" << endl;
+	cout << "2. Versla" << endl;
+	cin >> adgangur;
+
+	if (adgangur == 1)
+	{
+		cout << "Kennitala:";
+		cin >> kennitala;
+		adgangur = 2;
+	}
+	switch (adgangur){
+	case 2:
+		for (int i = 0; i < 10; i++)
+		{
+			shop[i].print();
+		}	
+		cout << "0. Checkout" << endl;
+		cin >> kaupa;
+
+		while (kaupa != 0){
+			switch (kaupa){
+
+			case 1:
+				cout << "Thu baettir vid pensil, Total: " << total << " kr." << endl;
+				for (int i = 0; i < 100; i++)
+				{
+					if (kennitala == mal.getKT())
+					{
+						mal.print();
+						total += (999 * 0.9);
+						break;
+					}
+				}
+				break;
+			case 2:
+				total += 1500;
+				cout << "Thu baettir vid 1L af Malningu, Total: " << total << " kr." << endl;
+				break;
+			case 3:
+				total += 6500;
+				cout << "Thu baettir vid 5L af Malningu, Total: " << total << " kr." << endl;
+				break;
+			case 4:
+				total += 11490;
+				cout << "Thu baettir vid 10L af Malningu, Total: " << total << " kr." << endl;
+				break;
+			case 5:
+				int metra;
+				cout << "Hversu marga metra? ";
+				cin >> metra;
+				total += 300 * metra;
+				cout << "Thu baettir vid " << metra << " metra af vir, Total: " << total << " kr." << endl;
+				break;
+			case 6:
+				total += 1500;
+				cout << "Thu baettir vid 5M Netsnuru, Total: " << total << " kr." << endl;
+				break;
+			case 7:
+				total += 2000;
+				cout << "Thu baettir vid 10M HDMI snuru, Total: " << total << " kr." << endl;
+				break;
+			case 8:
+				total += 500;
+				cout << "Thu baettir vid klo, Total: " << total << " kr." << endl;
+				break;
+			case 9:
+				total += 30000;
+				cout << "Thu baettir vid tenglarennu, Total: " << total << " kr." << endl;
+				break;
+			case 10:
+				total += 999;
+				cout << "Thu baettir vid Twix, Total: " << total << " kr." << endl;
+				break;
+			default:
+				cout << "Thu slost inn vitlaust";
+				break;
+			}
+			
+			cin >> kaupa;
+			
+		}
+		
+	};
+
+
+
+
+
+
+
+		cout << total << endl;
+		cout << "----------------" << endl;
+		
+
+		return 0;
+	}
 
